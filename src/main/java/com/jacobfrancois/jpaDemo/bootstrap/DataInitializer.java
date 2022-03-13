172c1044fa2c034e26f7da4486c317c9f51ee2db
Jacob Francois <jafranswa@gmail.com>
2022-03-13 16:07:51 -0600

    local app props cascading, integration tests, test splice

--- src/main/java/com/jacobfrancois/jpaDemo/bootstrap/DataInitializer.java
+++ src/main/java/com/jacobfrancois/jpaDemo/bootstrap/DataInitializer.java
@@ -3,8 +3,10 @@ package com.jacobfrancois.jpaDemo.bootstrap;
 import com.jacobfrancois.jpaDemo.model.Book;
 import com.jacobfrancois.jpaDemo.repository.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile({"local", "default"})
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -17,11 +19,18 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        bookRepository.deleteAll();
 
         Book book1 = new Book("hitchikers guide", "1234", "Megadodo Publications");
+        Book savedBook1 = bookRepository.save(book1);
+
         Book book2 = new Book("SQL never again", "4321", "never never land publications");
-        bookRepository.save(book1);
-        bookRepository.save(book2);
+        Book savedBook2 = bookRepository.save(book2);
+
+        bookRepository.findAll().forEach(book -> {
+            System.out.println("Book Id: " + book.getId());
+            System.out.println("Book Title: " + book.getTitle());
+        });
 
     }
 
---
+++ src/test/java/com/jacobfrancois/jpaDemo/MySqlIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.jacobfrancois.jpaDemo;
+
+
+import com.jacobfrancois.jpaDemo.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@ComponentScan(basePackages = "com.jacobfrancois.jpaDemo.bootstrap") //runs data initalizer to ensure data populates
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MySqlIntegrationTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testJpaTestSpliceTransaction() {
+        long beforeCount = bookRepository.count();
+        assertThat(beforeCount).isEqualTo(2);
+    }
+}
